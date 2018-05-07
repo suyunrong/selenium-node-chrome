01d4fae9532c653073fec165ba1d0e00472ecef3
suyunrong <475607014@qq.com>
2018-05-07 21:42:54 +0800

    update

--- NodeChrome/generate.sh
+++ NodeChrome/generate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-VERSION=latest
-NAMESPACE=suyunrong
+VERSION=3.11.0
+NAMESPACE=selenium
 AUTHORS=suyunrong
 
 echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" > ./Dockerfile
 echo "# NOTE: DO *NOT* EDIT THIS FILE.  IT IS GENERATED." >> ./Dockerfile
 echo "# PLEASE UPDATE Dockerfile.txt INSTEAD OF THIS FILE" >> ./Dockerfile
 echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" >> ./Dockerfile
-echo FROM $NAMESPACE/selenium-node-base:$VERSION >> ./Dockerfile
+echo FROM $NAMESPACE/node-base:$VERSION >> ./Dockerfile
 echo LABEL authors="$AUTHORS" >> ./Dockerfile
 echo "" >> ./Dockerfile
 cat ./Dockerfile.txt >> ./Dockerfile
